fa5c93f9dddf34060346623c102b7215d5acd9fb
ViktoryiaPiavets <pevets333@gmail.com>
2022-10-13 16:32:42 +0200

    arrays

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -233,7 +233,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {    
-  return arr.map((y, i) => arr.filter((t, j) => j <= i)).map(ii => ii.reduce((x, y) => x + y, 0));
+  return arr.reduce((acc, cur, idx) => [...acc, cur + (acc[idx - 1] || 0)], []);
 }
 
 /**
